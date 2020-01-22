f4858adf8609cf07ff9e2e59d527a54ea3cd7e63
Dannie Schumaker <draeschumaker@gmail.com>
2020-01-22 09:07:44 -0800

    write example list and object literals

---
+++ data/coffee.js
@@ -0,0 +1,88 @@
+const v60 = {
+    id: 'v60',
+    name: 'Hario V60',
+    image: '../assets/v60.jpg',
+    description: 'The most popular method for control-obsessed coffee snobs.',
+    category: 'Pour Over',
+    price: 20.00,
+};
+
+const chemex = {
+    id: 'chemex',
+    name: 'Chemex',
+    image: '../assets/chemex.jpg',
+    description: 'Slightly more user-friendly than the V60.',
+    category: 'Pour Over',
+    price: 30.00,
+};
+
+const aeropress = {
+    id: 'aeropress',
+    name: 'Aerobie Aeropress',
+    image: '../assets/aeropress.jpg',
+    description: 'Versatile and portable. No gooseneck kettle required!',
+    category: 'Immersion',
+    price: 25.00,
+};
+
+const clever = {
+    id: 'clever',
+    name: 'Clever Coffee Dripper',
+    image: '../assets/clever.jpg',
+    description: 'A cleaner-tasting alternative to french press.',
+    category: 'Immersion',
+    price: 20.00,
+};
+
+const frenchPress = {
+    id: 'french-press',
+    name: 'French Press',
+    image: '../assets/french-press.jpg',
+    description: 'Full-bodied, easy to brew and yielding consistent results, this is the most widely used brew method.',
+    category: 'Immersion',
+    price: 20.00,
+};
+
+const kalita = {
+    id: 'kalita',
+    name: 'Kalita Wave',
+    image: '../assets/kalita.jpg',
+    description: 'Use multiple pours to yield a clear and complex brew.',
+    category: 'Pour Over',
+    price: 40.00,
+};
+
+const siphon = {
+    id: 'siphon',
+    name: 'Siphon',
+    image: '../assets/siphon.jpg',
+    description: 'Are you the coolest person you know? The siphon is the perfect super-hipster option for you.',
+    category: 'Pour Over',
+    price: 82.00,
+};
+
+const coldBrew = {
+    id: 'cold-brew',
+    name: 'Hario Mizudashi Cold Brew',
+    image: '../assets/cold-brew.jpg',
+    description: 'Brew coarsely ground coffee slowly in cold water with this method. Heavy-bodied.',
+    category: 'Cold Brew',
+    price: 35.00,
+};
+
+
+const coffee = [
+    v60,
+    chemex,
+    aeropress,
+    clever,
+    frenchPress,
+    kalita,
+    siphon,
+    coldBrew,
+];
+
+
+
+
+export default coffee;
---
+++ test/tests.js
@@ -0,0 +1,1 @@
+import './render-coffee.test.js';
